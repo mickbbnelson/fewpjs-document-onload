852221b27f9c01da811bf64fde615486e3f5a7e6
mickbbnelson <75447451+mickbbnelson@users.noreply.github.com>
2021-08-31 14:51:18 -0500

    commit tree e65588a72f280832b72eb1a2b66aebfa8621841d

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function() {
+    const sentence = document.getElementById("text");
+    sentence.innerHTML = "This is really cool!"
+  }); 
+  
